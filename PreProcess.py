88413978a7b9a4ea880f336e338fed8152efab07
Sebastian Rivera <sebastan12grc@gmail.com>
2022-10-18 10:55:35 -0700

    Calculate Total Scores and creating 'genres' column

--- PreProcess.py
+++ PreProcess.py
@@ -26,14 +26,55 @@ def extract_year(date):
 df['year'] = df['release_date'].apply(extract_year)
 
 #Create score column
-def totalRatings(row):
-    posCount = row['positive_ratings']
-    negCount = row['negative_ratings']
-    totalCount = posCount + negCount
-    return posCount, negCount, totalCount
-def createScore(row):
-    posCount, negCount, totalCount = totalRatings(row)
-    average = posCount / totalCount
+def create_score(row):
+    pos_count = row['positive_ratings']
+    neg_count = row['negative_ratings']
+    total_count = pos_count + neg_count
+    average = pos_count / total_count
     return round(average, 2)
-df['total_ratings'] = df.apply(totalRatings[2], axis=1)
-df['score'] = df.apply(createScore, axis=1)
+def total_ratings(row):
+    pos_count = row['positive_ratings']
+    neg_count = row['negative_ratings']
+    total_count = pos_count + neg_count
+    return total_count
+df['total_ratings'] = df.apply(total_ratings, axis=1)
+df['score'] = df.apply(create_score, axis=1)
+
+# Calculate mean of vote average column
+C = df['score'].mean()
+# Calculate the minimum number of votes required to be in the chart
+m = df['total_ratings'].quantile(0.90)
+print(m)
+
+# calculate the weighted rating for each qualified game
+# Function that computes the weighted rating of each game
+def weighted_rating(x, m=m, C=C):
+    v = x['total_ratings']
+    R = x['score']
+    # Calculation based on the IMDB formula
+    return round((v/(v+m) * R) + (m/(m+v) * C), 2)
+# Define a new feature 'score' and calculate its value with `weighted_rating()`
+df['weighted_score'] = df.apply(weighted_rating, axis=1)
+#Print the top 15 games
+print(df[['name', 'total_ratings', 'score', 'weighted_score']].head(15))
+
+#We're adding this is for tags with multiple words, we need to connect by '-' before we split them by ' '
+df['steamspy_tags'] = df['steamspy_tags'].str.replace(' ','-')
+#TF-IDF Vectorizer further down will identify the words by the spaces between the words
+df['genres'] = df['steamspy_tags'].str.replace(';',' ')
+# count the number of occurences for each genre in the data set
+counts = dict()
+for i in df.index:
+#for each element in list (each row, split by ' ', in genres column)
+#-- we're splitting by space so tfidf can interpret the rows
+    for g in df.loc[i,'genres'].split(' '):
+    #if element is not in counts(dictionary of genres)
+        if g not in counts:
+            #give genre dictonary entry the value of 1
+            counts[g] = 1
+        else:
+            #increase genre dictionary entry by 1
+            counts[g] = counts[g] + 1
+#Test Genre Counts
+counts.keys()
+print(counts['Action'])
