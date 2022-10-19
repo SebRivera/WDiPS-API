4dda2b7359b810c98208482f609b7f8745f16fc9
Sebastian Rivera <sebastan12grc@gmail.com>
2022-10-19 16:35:57 -0700

    Put all the preProcess into funtions, Created Recommender.py and created the basic recommender

--- PreProcess.py
+++ PreProcess.py
@@ -2,79 +2,84 @@ import pandas as pd
 import numpy as np
 import re
 import itertools
-# import matplotlib.pyplot as plt
+## import matplotlib.pyplot as plt
+def importData(filename):
+    df = pd.read_csv(filename, encoding = "utf-8")
+    ##fileName = "Data\steam.csv"
+    return df
 
-fileName = "Data\steam.csv"
-df = pd.read_csv(fileName,  error_bad_lines=False, encoding='utf-8')
 #These are just quick checks to make sure the dataset looks correct
-print(df.shape)
-df.head()
+## print(df.shape)
+## df.head()
 
 #Which columns have null values?
-print(df.columns[df.isna().any()].tolist())
+##print(df.columns[df.isna().any()].tolist())
 #How many null values per column? - Count the missing values in each column
-df.isnull().sum()
-print("TEst")
+##df.isnull().sum()
 
 # Extracts the year from the release date
-def extract_year(date):
+def extractYear(date):
     year = date[:4]
     if year.isnumeric():
         return int(year)
     else:
         return np.nan
-df['year'] = df['release_date'].apply(extract_year)
 
-#Create score column
-def create_score(row):
-    pos_count = row['positive_ratings']
-    neg_count = row['negative_ratings']
-    total_count = pos_count + neg_count
-    average = pos_count / total_count
+def totalRatings(row):
+    posCount = row['positive_ratings']
+    negCount = row['negative_ratings']
+    totalCount = posCount + negCount
+    return totalCount
+def createScore(row):
+    posCount = row['positive_ratings']
+    negCount = row['negative_ratings']
+    totalCount = posCount + negCount
+    average = posCount / totalCount
     return round(average, 2)
-def total_ratings(row):
-    pos_count = row['positive_ratings']
-    neg_count = row['negative_ratings']
-    total_count = pos_count + neg_count
-    return total_count
-df['total_ratings'] = df.apply(total_ratings, axis=1)
-df['score'] = df.apply(create_score, axis=1)
 
-# Calculate mean of vote average column
-C = df['score'].mean()
-# Calculate the minimum number of votes required to be in the chart
-m = df['total_ratings'].quantile(0.90)
-print(m)
+def addScoreAndTotalRatings(df):
+    df['score'] = df.apply(createScore, axis=1)
+    df['total_ratings'] = df.apply(totalRatings, axis=1)
+    return df
+
 
 # calculate the weighted rating for each qualified game
 # Function that computes the weighted rating of each game
-def weighted_rating(x, m=m, C=C):
+def weighted_rating(x, m, C):
     v = x['total_ratings']
     R = x['score']
     # Calculation based on the IMDB formula
     return round((v/(v+m) * R) + (m/(m+v) * C), 2)
-# Define a new feature 'score' and calculate its value with `weighted_rating()`
-df['weighted_score'] = df.apply(weighted_rating, axis=1)
-#Print the top 15 games
-print(df[['name', 'total_ratings', 'score', 'weighted_score']].head(15))
 
-#We're adding this is for tags with multiple words, we need to connect by '-' before we split them by ' '
-df['steamspy_tags'] = df['steamspy_tags'].str.replace(' ','-')
-#TF-IDF Vectorizer further down will identify the words by the spaces between the words
-df['genres'] = df['steamspy_tags'].str.replace(';',' ')
-# count the number of occurences for each genre in the data set
-counts = dict()
-for i in df.index:
-#for each element in list (each row, split by ' ', in genres column)
-#-- we're splitting by space so tfidf can interpret the rows
-    for g in df.loc[i,'genres'].split(' '):
-    #if element is not in counts(dictionary of genres)
-        if g not in counts:
-            #give genre dictonary entry the value of 1
-            counts[g] = 1
-        else:
-            #increase genre dictionary entry by 1
-            counts[g] = counts[g] + 1
-#Test Genre Counts
-counts.keys()
-print(counts['Action'])
+def addWeightedRating(df):
+    # Calculate mean of vote average column
+    C = df['score'].mean()
+    # Calculate the minimum number of votes required to be in the chart
+    m = df['total_ratings'].quantile(0.90)
+    # Define a new feature 'score' and calculate its value with `weighted_rating()`
+    df['weighted_score'] = df.apply(weighted_rating, axis=1, args=(m, C))
+    return df
+
+
+def formatColumns(df):
+    #We're adding this is for tags with multiple words, we need to connect by '-' before we split them by ' '
+    df['steamspy_tags'] = df['steamspy_tags'].str.replace(' ','-')
+    #TF-IDF Vectorizer further down will identify the words by the spaces between the words
+    df['genres'] = df['steamspy_tags'].str.replace(';',' ')
+    # count the number of occurences for each genre in the data set
+    counts = dict()
+    for i in df.index:
+    #for each element in list (each row, split by ' ', in genres column)
+    #-- we're splitting by space so tfidf can interpret the rows
+        for g in df.loc[i,'genres'].split(' '):
+        #if element is not in counts(dictionary of genres)
+            if g not in counts:
+                #give genre dictonary entry the value of 1
+                counts[g] = 1
+            else:
+                #increase genre dictionary entry by 1
+                counts[g] = counts[g] + 1
+    #Test Genre Counts
+    counts.keys()
+    print(counts['Action'])
+    return df
---
+++ Recommender.py
@@ -0,0 +1,103 @@
+import PreProcess
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from fuzzywuzzy import fuzz
+
+def matching_score(a,b):
+   return fuzz.ratio(a,b)
+   # exactly the same, the score becomes 100
+   
+def get_title_year_from_index(df, index):
+   return df[df.index == index]['year'].values[0]
+def get_title_from_index(df, index):
+   return df[df.index == index]['name'].values[0]
+def get_index_from_title(df, title):
+   return df[df.name == title].index.values[0]
+def get_score_from_index(df, index):
+   return df[df.index == index]['score'].values[0]
+def get_weighted_score_from_index(df, index):
+   return df[df.index == index]['weighted_score'].values[0]
+def get_total_ratings_from_index(df, index):
+   return df[df.index == index]['total_ratings'].values[0]
+def get_platform_from_index(df, index):
+   return df[df.index == index]['platforms'].values[0]
+
+# A function to return the most similar title to the words a user type
+# Without this, the recommender only works when a user enters the exact title which the data has.
+def find_closest_title(df, title):
+    leven_scores = list(enumerate(df['name'].apply(matching_score, b=title))) #[(0, 30), (1,95), (2, 19)~~] A tuple of distances per index
+    sorted_leven_scores = sorted(leven_scores, key=lambda x: x[1], reverse=True) #Sorts list of tuples by distance [(1, 95), (3, 49), (0, 30)~~]
+    closest_title = get_title_from_index(df, sorted_leven_scores[0][0])
+    distance_score = sorted_leven_scores[0][1]
+    return closest_title, distance_score
+# returns only one title but I want a dropdown of the 10 closest game titles
+def closestNames(title):
+    df = pd.DataFrame()
+    leven_scores = list(enumerate(df['name'].apply(matching_score, b=title)))
+    sorted_leven_scores = sorted(leven_scores, key=lambda x: x[1], reverse=True)
+    top_closest_names = [get_title_from_index(i[0]) for i in sorted_leven_scores[:10]]
+    return top_closest_names
+    
+def gradio_contents_based_recommender_v2(df, game, how_many, dropdown_option, sort_option, min_year, platform, min_score):
+    #Return closest game title match
+    closest_title, distance_score = find_closest_title(df, dropdown_option)
+    #Create a Dataframe with these column headers
+    recomm_df = pd.DataFrame(columns=['Game Title', 'Year', 'Score', 'Weighted Score', 'Total Ratings'])
+    #Make the closest title whichever dropdown option the user has chosen
+    closest_title = dropdown_option
+    #find the corresponding index of the game title
+    games_index = get_index_from_title(df, closest_title)
+    #return a list of the most similar game indexes as a list
+    games_list = list(enumerate(sim_matrix[int(games_index)]))
+    #Sort list of similar games from top to bottom
+    similar_games = list(filter(lambda x:x[0] != int(games_index), sorted(games_list,key=lambda x:x[1], reverse=True)))
+    #Print the game title the similarity matrix is based on
+    print('Here\'s the list of games similar to '+'\033[1m'+str(closest_title)+'\033[0m'+'.\n')
+    #Only return the games that are on selected platform
+    n_games = []
+    for i,s in similar_games:
+        if platform in get_platform_from_index(df, i):
+            n_games.append((i,s))
+    #Only return the games that are above the minimum score
+    high_scores = []
+    for i,s in n_games:
+        if get_score_from_index(df, i) > min_score:
+            high_scores.append((i,s))
+    #Return the game tuple (game index, game distance score) and store in a dataframe
+    for i,s in n_games[:how_many]:
+        #Dataframe will contain attributes based on game index
+        row = {'Game Title': get_title_from_index(df, i), 'Year': get_title_year_from_index(df, i), 'Score': get_score_from_index(df, i), 'Weighted Score': get_weighted_score_from_index(df, i), 'Total Ratings': get_total_ratings_from_index(df,i),}
+        #Append each row to this dataframe
+        recomm_df = recomm_df.append(row, ignore_index = True)
+    #Sort dataframe by Sort_Option provided by 
+    userrecomm_df = recomm_df.sort_values(sort_option, ascending=False)
+    #Only include games released same or after minimum year 
+    selectedrecomm_df = recomm_df[recomm_df['Year'] >= min_year]
+    return recomm_df
+
+if __name__ == '__main__':
+    # Apply the PreProcess functions to the dataset
+    dataDF = PreProcess.importData("Data\steam.csv")
+    dataDF['year'] = dataDF['release_date'].apply(PreProcess.extractYear)
+    dataDF = PreProcess.addScoreAndTotalRatings(dataDF)
+    dataDF = PreProcess.addWeightedRating(dataDF)
+    
+    #Print the top 15 games
+    print(dataDF[['name', 'total_ratings', 'score', 'weighted_score']].head(15))
+    
+    dataDF = PreProcess.formatColumns(dataDF)
+    
+    # create an object for TfidfVectorizer
+    tfidfVector = TfidfVectorizer(stop_words='english')
+    # apply the object to the genres column
+    # convert the list of documents (rows of genre tags) into a matrix
+    tfidfMatrix = tfidfVector.fit_transform(dataDF['genres'])
+    print(tfidfMatrix.shape)
+    
+    # create the cosine similarity matrix
+    sim_matrix = linear_kernel(tfidfMatrix,tfidfMatrix)
+    print(sim_matrix)
+    result = gradio_contents_based_recommender_v2(dataDF, "Terraria", 10, "Terraria", "Score", 2000, "windows", 0)
+    
+    print(result)
