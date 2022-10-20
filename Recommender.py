e0122d450bad1ce21a02d913fc1bd043fd5caadd
Sebastian Rivera <sebastan12grc@gmail.com>
2022-10-20 08:39:02 -0700

    Replaced .append with .concat to avoid future warning.

--- Recommender.py
+++ Recommender.py
@@ -67,9 +67,9 @@ def gradio_contents_based_recommender_v2(df, game, how_many, dropdown_option, so
     #Return the game tuple (game index, game distance score) and store in a dataframe
     for i,s in n_games[:how_many]:
         #Dataframe will contain attributes based on game index
-        row = pd.DataFrame({'Game Title': get_title_from_index(df, i), 'Year': get_title_year_from_index(df, i), 'Score': get_score_from_index(df, i), 'Weighted Score': get_weighted_score_from_index(df, i), 'Total Ratings': get_total_ratings_from_index(df,i),})
+        row = pd.DataFrame({'Game Title': get_title_from_index(df, i), 'Year': get_title_year_from_index(df, i), 'Score': get_score_from_index(df, i), 'Weighted Score': get_weighted_score_from_index(df, i), 'Total Ratings': get_total_ratings_from_index(df,i)}, index = [0])
         #Append each row to this dataframe
-        pd.concat([row, recomm_df], ignore_index=True)
+        recomm_df = pd.concat([row, recomm_df])
         #recomm_df = recomm_df.concat(row, ignore_index = True)
     #Sort dataframe by Sort_Option provided by 
     userrecomm_df = recomm_df.sort_values(sort_option, ascending=False)
